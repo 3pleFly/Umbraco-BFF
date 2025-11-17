fa3d0a5050bee751908fb13cb4ba928be364372c
Freddy Goodenough <freddy@Freddys-MacBook-Air.local>
2025-11-17 18:05:28 +0200

    fixed properties, removed unused

--- Form.Domain/ContentItem.cs
+++ Form.Domain/ContentItem.cs
@@ -2,25 +2,11 @@ namespace Form.Domain;
 
 public class ContentItem
 {
-    public string ContentType { get; set; }
-    public string Name { get; set; }
+    public required string Id { get; set; }
+    public required string ContentType { get; set; }
+    public required string Name { get; set; }
     public DateTime CreateDate { get; set; }
     public DateTime UpdateDate { get; set; }
-    public ContentItemRoute Route { get; set; }
-    public string Id { get; set; }
-    public Dictionary<string, object> Properties { get; set; }
-    public Dictionary<string, object> Cultures { get; set; }
-}
-
-public class ContentItemRoute
-{
-    public string Path { get; set; }
-    public string QueryString { get; set; }
-    public ContentItemStart StartItem { get; set; }
-}
-
-public class ContentItemStart
-{
-    public string Id { get; set; }
-    public string Path { get; set; }
+    public required object Route { get; set; }
+    public Dictionary<string, object> Properties { get; set; } = [];
 }
